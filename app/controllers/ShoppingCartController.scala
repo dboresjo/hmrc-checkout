36165949907a5c808bbae57dbb8b167cf53a2555
Dan Boresjö <dan.boresjo@white-bullet.com>
2023-11-02 11:40:20 +0000

    naive implementation for an apple

--- app/controllers/ShoppingCartController.scala
+++ app/controllers/ShoppingCartController.scala
@@ -1,13 +1,18 @@
 package controllers
 
-import play.api.libs.json.JsNumber
+import play.api.libs.json.{JsArray, JsNumber, JsString}
 import play.api.mvc.{Action, AnyContent, InjectedController}
 
 import javax.inject._
 
 @Singleton
 class ShoppingCartController extends InjectedController {
-  def calculateCost(): Action[AnyContent] = Action {
-    Ok(JsNumber(0))
+  def calculateCost(): Action[AnyContent] = Action { request =>
+    val items = request.body.asJson.get.as[JsArray].value.map{_.as[String]}.toSeq
+    val cost = items match {
+      case Seq("Apple") => 60
+      case Seq() => 0
+    }
+    Ok(JsNumber(cost))
   }
 }
