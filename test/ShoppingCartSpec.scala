fa22c9b59f069ed20c78e797931e4405ddede726
Dan Boresjö <dan.boresjo@white-bullet.com>
2023-11-02 10:57:50 +0000

    initial test

---
+++ test/ShoppingCartSpec.scala
@@ -0,0 +1,16 @@
+import org.specs2.execute.Results
+import play.api.libs.json.{JsArray, JsNumber, JsString}
+import play.api.test.{FakeRequest, PlaySpecification, WithApplication}
+
+class ShoppingCartSpec extends PlaySpecification with Results {
+  "calculate-cost" should {
+    "return zero for zero items" in new WithApplication {
+      val givenItems = JsArray(Seq());
+
+      val result = route(app, FakeRequest(GET, "/calculate-cost").withBody(givenItems)).get
+
+      status(result) must equalTo(OK)
+      contentAsJson(result) must equalTo(JsNumber(0))
+    }
+  }
+}
