f7c0f8038239d9f8b9e8eaed79006ecceec2a904
Dan Boresjö <dan.boresjo@white-bullet.com>
2023-11-02 11:40:02 +0000

    test for an apple

--- test/ShoppingCartSpec.scala
+++ test/ShoppingCartSpec.scala
@@ -1,5 +1,5 @@
 import org.specs2.execute.Results
-import play.api.libs.json.{JsArray, JsNumber}
+import play.api.libs.json.{JsArray, JsNumber, JsString}
 import play.api.test.{FakeRequest, PlaySpecification, WithApplication}
 
 class ShoppingCartSpec extends PlaySpecification with Results {
@@ -11,6 +11,15 @@ class ShoppingCartSpec extends PlaySpecification with Results {
 
       status(result) must equalTo(OK)
       contentAsJson(result) must equalTo(JsNumber(0))
+    }
+
+    "return 60 for an apple" in new WithApplication {
+      val givenItems = JsArray(Seq(JsString("Apple")))
+
+      val result = route(app, FakeRequest(GET, "/calculate-cost").withBody(givenItems)).get
+
+      status(result) must equalTo(OK)
+      contentAsJson(result) must equalTo(JsNumber(60))
     }
   }
 }
